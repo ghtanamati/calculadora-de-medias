5a81cee57efc32813f9126ba1e4f9cd8a5216fd2
Gustavo Hideki Tanamati <112647092+ghtanamati@users.noreply.github.com>
2022-11-10 13:46:16 -0300

    Upload calculadora-de-medias
    Upload Desafio C# - TI Academy + Benner - Calculadora de Médias

---
+++ calculadora-de-medias/Program.cs
@@ -0,0 +1,42 @@
+﻿int j = 1;
+
+Console.WriteLine("Seja bem-vindo à calculadora de médias!");
+
+do
+{
+    int numNotas = 0;
+    float somaTotal = 0;
+    float notaParcial = 0;
+    double mediaFinal = 0;
+
+    Console.WriteLine("Digite a quantidade de notas a ser calculada");
+    numNotas = int.Parse(Console.ReadLine());
+
+    for (int i = 1; i <= numNotas; i++)
+    {
+        Console.WriteLine("Digite a nota " + i +":");
+        notaParcial = float.Parse(Console.ReadLine());
+        somaTotal = somaTotal + notaParcial;
+    }
+
+    mediaFinal = somaTotal/numNotas;
+
+    Console.WriteLine("A média do aluno foi: "+mediaFinal);
+
+    if(mediaFinal >= 6)
+    {
+        Console.WriteLine("Parabéns, você foi aprovado");
+    }
+    else
+    {
+        Console.WriteLine("Você está de recuperação, estude mais!");        
+    }
+
+    Console.WriteLine(@"Deseja calcular mais médias?
+    Sim - Digite 1
+    Não - Digite 0");
+    j = int.Parse(Console.ReadLine());
+
+} while (j == 1);
+
+Console.WriteLine("Obrigado por utilizar a calculadora de médias!");
